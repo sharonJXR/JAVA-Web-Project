bac22fc6c6b333b7c6f26354729317045290e7f8
sharonJXR <llarso74@stu.jjc.edu>
2017-12-04 21:17:03 +0800

    Seventh commit

--- BookSttore/src/com/ioii/bookstore/web/controller/UserServlet.java
+++ BookSttore/src/com/ioii/bookstore/web/controller/UserServlet.java
@@ -42,7 +42,7 @@ public class UserServlet extends HttpServlet {
 			Account account) throws ServletException, IOException{
 		List<Account> list = ServiceFactory.getService("user").findByConditions(account.getAccount());
 		if(list.isEmpty() || !list.get(0).getPassword().equals(account.getPassword())){
-			req.setAttribute("login_err", "用户名或密码错误!");
+			req.setAttribute("login_err", "Unknown user name or bad password!");
 			forward(req, resp,"/pages/login_register.jsp");
 			return;
 		}
@@ -57,13 +57,13 @@ public class UserServlet extends HttpServlet {
 		String email = req.getParameter("email").trim();
 		account.setEmail(email);	
 		if("".equals(name)){
-			req.setAttribute("name_err", "用户名不能为空!");
+			req.setAttribute("name_err", "The username cannot be empty!");
 			forward(req, resp,"/pages/login_register.jsp");
 			return;
 		}
 		List<Account> list = ServiceFactory.getService("user").findByConditions(name);
 		if(!list.isEmpty()){
-			req.setAttribute("name_err", "用户名已存在!");
+			req.setAttribute("name_err", "The username has already existed!");
 			forward(req, resp,"/pages/login_register.jsp");
 			return;
 		}
